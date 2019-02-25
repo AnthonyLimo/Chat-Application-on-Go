43d171657bd716176ab44ddb3bb97628f1d51a79
Anthony Kiplimo <anthonylimo90@gmail.com>
2019-02-25 12:45:10 +0300

    Creating room functionality and support for concurrency

--- room.go
+++ room.go
@@ -4,4 +4,29 @@ type room struct {
 	//forward is a channel that holds incoming messages
 	//that should be forwarded to other clients
 	forward chan []byte
+	//Join is channel for clients wishing to join the room
+	join chan *client
+	//Leave is a channel for clients wishing to leave the room
+	leave chan *client
+	//Clients holds all current clients in this room
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			//joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			//leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			//forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
 }
